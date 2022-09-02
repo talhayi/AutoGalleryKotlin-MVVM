16ba2752370e642b6e51e2e20ce9dee96543f02b
talhayi <talhayldrm23@gmail.com>
2022-09-02 16:38:07 +0300

    Register UI created

--- app/src/main/java/com/example/autogallerykotlin/RegisterActivity.kt
+++ app/src/main/java/com/example/autogallerykotlin/RegisterActivity.kt
@@ -3,11 +3,9 @@ package com.example.autogallerykotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
     }
-
-
 }
