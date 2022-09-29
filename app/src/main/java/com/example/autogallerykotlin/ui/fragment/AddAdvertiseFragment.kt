e23a13a84332f246758a71ff690fd42d56a8bcd2
talhayi <talhayldrm23@gmail.com>
2022-09-29 21:03:10 +0300

    AdvertiseTitle AlertDialog created

--- app/src/main/java/com/example/autogallerykotlin/ui/fragment/AddAdvertiseFragment.kt
+++ app/src/main/java/com/example/autogallerykotlin/ui/fragment/AddAdvertiseFragment.kt
@@ -1,40 +1,59 @@
 package com.example.autogallerykotlin.ui.fragment
 
-import android.content.Intent
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.navigation.Navigation
+import android.widget.EditText
+import android.widget.TextView
 import com.example.autogallerykotlin.R
 import com.example.autogallerykotlin.databinding.FragmentAddAdvertiseBinding
-import com.example.autogallerykotlin.ui.view.MainActivity
 
 class AddAdvertiseFragment : Fragment() {
 
     private var _binding: FragmentAddAdvertiseBinding? = null
-    private val binding get()=_binding!!
+    private val binding get() = _binding!!
 
+    private lateinit var alertDialog: AlertDialog.Builder
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        _binding = FragmentAddAdvertiseBinding.inflate(inflater,container,false)
-        return binding?.root
+        _binding = FragmentAddAdvertiseBinding.inflate(inflater, container, false)
+        return binding.root
 
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
-        binding.ilanBasliILinearLayout.setOnClickListener {
-            Toast.makeText(requireContext(), "clicked", Toast.LENGTH_SHORT).show()
+
+
+        binding.advertiseTitleLinearLayout.setOnClickListener {
+
+            val mDialogView = LayoutInflater.from(requireContext())
+                .inflate(R.layout.advertise_title_alert_dialog, null)
+            alertDialog = AlertDialog.Builder(requireContext()).setView(mDialogView)
+
+            alertDialog.setNegativeButton("İPTAL") { _, _ -> }
+
+            alertDialog.setPositiveButton("TAMAM") { _, _ ->
+
+                val advertiseTitleDialog =
+                    mDialogView.findViewById<EditText>(R.id.advertiseTitleDialogEditText).text.toString()
+                        .trim()
+                binding.advertiseTitleTextView.text = advertiseTitleDialog
+
+            }
+
+            alertDialog.show()
+
         }
+
 /*
         binding.addAdvertiseButton.setOnClickListener {
 
@@ -49,6 +68,7 @@ class AddAdvertiseFragment : Fragment() {
             Navigation.findNavController(view).navigate(action)
         }*/
 
-
     }
+
 }
+
