9bc890c4df202154aa1481c28aba35c055f18c1b
talhayi <talhayldrm23@gmail.com>
2022-10-11 16:24:03 +0300

    UploadImagesFragment design bug fixed

--- app/src/main/java/com/example/autogallerykotlin/ui/fragment/UploadImagesFragment.kt
+++ app/src/main/java/com/example/autogallerykotlin/ui/fragment/UploadImagesFragment.kt
@@ -1,31 +1,33 @@
 package com.example.autogallerykotlin.ui.fragment
 
+import android.Manifest
 import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.Color
 import android.graphics.ImageDecoder
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
-import androidx.fragment.app.Fragment
+import android.util.Base64
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.autogallerykotlin.R
 import com.example.autogallerykotlin.databinding.FragmentUploadImagesBinding
 import com.google.android.material.snackbar.Snackbar
+import java.io.ByteArrayOutputStream
 import java.io.IOException
-import android.Manifest
-import android.graphics.Color
-import android.view.Gravity
-import androidx.coordinatorlayout.widget.CoordinatorLayout
-import com.example.autogallerykotlin.R
 
 
 class UploadImagesFragment : Fragment() {
@@ -33,7 +35,7 @@ class UploadImagesFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
     private lateinit var permissionLauncher: ActivityResultLauncher<String>
-    private var selectedBitmap: Bitmap? = null
+    private var bitmap: Bitmap? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -70,6 +72,7 @@ class UploadImagesFragment : Fragment() {
                         permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
 
                     }.show()
+                  //  binding.uploadImageView.visibility = View.VISIBLE
                     val params = snackbar.view.layoutParams as CoordinatorLayout.LayoutParams
                     params.anchorId = R.id.navigationBottomBar //id of the bottom navigation view
 
@@ -106,13 +109,15 @@ class UploadImagesFragment : Fragment() {
 
                                 val source = ImageDecoder.createSource(requireActivity().contentResolver, imageData!!)
 
-                                selectedBitmap = ImageDecoder.decodeBitmap(source)
-
-                                binding.imageView.setImageBitmap(selectedBitmap)
+                                bitmap = ImageDecoder.decodeBitmap(source)
+                                binding.uploadImageView.setImageBitmap(bitmap)
+                                binding.uploadImageView.visibility = View.VISIBLE
                             }
                             else {
-                                selectedBitmap = MediaStore.Images.Media.getBitmap(requireActivity().contentResolver, imageData)
-                                binding.imageView.setImageBitmap(selectedBitmap)
+
+                                bitmap = MediaStore.Images.Media.getBitmap(requireActivity().contentResolver, imageData)
+                                binding.uploadImageView.setImageBitmap(bitmap)
+                                binding.uploadImageView.visibility = View.VISIBLE
                             }
                         } catch (e: IOException) {
                             e.printStackTrace()
@@ -132,6 +137,16 @@ class UploadImagesFragment : Fragment() {
                     Toast.makeText(requireContext(), "İzin gerekiyor", Toast.LENGTH_LONG).show()
                 }
             }
+    }
+
+    private fun imageToString(): String {
+
+        // resmi base64 Stringine çevirmek için
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        bitmap!!.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+        val byt = byteArrayOutputStream.toByteArray()
+        return Base64.encodeToString(byt, Base64.DEFAULT)
+
     }
 
     private fun makeSmallerBitmap(image: Bitmap, maximumSize : Int) : Bitmap {
